5b6ed717bba3e84da8f57bdb366cdb4074458c5c
sagudev <16504129+sagudev@users.noreply.github.com>
2022-08-07 10:55:09 +0200

    lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+use itertools::Itertools;
+use urlencoding::decode;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if let Some(filename) = args.get(1) {
+        let mut file = File::open(&filename).expect("Unable to open file");
+        let mut data = String::new();
+        file.read_to_string(&mut data)
+            .expect("Unable to read string");
+        let data: String = data.lines().map(|x| decode(x).expect("UTF-8")).join("\n");
+        println!("{}", data);
+    } else {
+        panic!("NO file provided!")
+    }
 }
